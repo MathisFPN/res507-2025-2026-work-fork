805dc0483059b803703b093bdd465577f0ff9c93
Frappinm <mathis74f@gmail.com>
2026-02-05 09:25:22 +0100

    move tests

--- labs/40-containerized-node-app/app/tests/page.test.js
+++ labs/40-containerized-node-app/app/tests/page.test.js
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import { buildApp } from '../app/app.js';
+import { buildApp } from '../app.js';
 
 test('GET / responds with a page', async () => {
   const app = await buildApp();
